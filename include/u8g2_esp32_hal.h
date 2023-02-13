c662d7dc38e244a4bd10e167151d28b9661ab6e0
r12f <r12f.code@gmail.com>
2023-02-12 21:39:49 -0800

    Adding c++ support and fix more compile failures.

--- include/u8g2_esp32_hal.h
+++ include/u8g2_esp32_hal.h
@@ -15,19 +15,22 @@
 #include "driver/i2c.h"
 #include "driver/spi_master.h"
 
-#define U8G2_ESP32_HAL_UNDEFINED (-1)
+#define U8G2_ESP32_HAL_UNDEFINED ((gpio_num_t)-1)
 
-#if SOC_I2C_NUM > 1
-#define I2C_MASTER_NUM I2C_NUM_1     //  I2C port number for master dev
-#else
 #define I2C_MASTER_NUM I2C_NUM_0     //  I2C port number for master dev
-#endif
 
 #define I2C_MASTER_TX_BUF_DISABLE 0  //  I2C master do not need buffer
 #define I2C_MASTER_RX_BUF_DISABLE 0  //  I2C master do not need buffer
 #define I2C_MASTER_FREQ_HZ 50000     //  I2C master clock frequency
 #define ACK_CHECK_EN 0x1             //  I2C master will check ack from slave
 #define ACK_CHECK_DIS 0x0  //  I2C master will not check ack from slave
+
+/*==========================================*/
+/* C++ compatible */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct {
   gpio_num_t clk;
@@ -60,6 +63,14 @@ uint8_t u8g2_esp32_gpio_and_delay_cb(u8x8_t* u8x8,
                                      uint8_t msg,
                                      uint8_t arg_int,
                                      void* arg_ptr);
+
+/*==========================================*/
+/* C++ compatible */
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* U8G2_ESP32_HAL_H_ */
 
 #endif
